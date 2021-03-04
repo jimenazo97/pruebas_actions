a18da40c6c9753e9fe05bbe9b44a3016ff50cdb6
jimenazo97 <32390728+jimenazo97@users.noreply.github.com>
2021-03-04 08:54:47 +0100

    Update prueba.py

--- .github/workflows/prueba.py
+++ .github/workflows/prueba.py
@@ -1,1 +1,28 @@
-print ("Hola")
+import mysql.connector
+import sys
+import git
+import os
+import yaml
+
+ 
+
+path=sys.argv[1]
+print ("path:  "+path)
+
+ 
+
+repo = git.Repo(path)
+hcommit = repo.head.commit
+for diff_added in hcommit.diff("HEAD~1"):
+    fileChanged=str(diff_added).split("==")[0].rstrip()
+    ruta = path+"/"+fileChanged
+    print("fichero->  "+path+"/"+fileChanged)
+
+ 
+
+    with open(ruta, 'r') as file:
+        field_list = yaml.safe_load(file)
+
+ 
+
+        print(field_list)
