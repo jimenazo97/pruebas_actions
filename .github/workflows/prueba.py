bff23218feaa629ff52edd9cac56d343f9ae891c
Jimenazo97 <jimenazo97@gmail.com>
2021-03-05 09:57:26 +0100

    uhh

--- .github/workflows/prueba.py
+++ .github/workflows/prueba.py
@@ -10,16 +10,15 @@ import yaml
 #print ("path:  "+path)
 
  
-
-repo = git.Repo("/Users/andresjimenez/Documents/GitHub/pruebas_actions/validaciones")
+path="/Users/andresjimenez/Documents/GitHub/pruebas_actions"
+repo = git.Repo("/Users/andresjimenez/Documents/GitHub/pruebas_actions")
 hcommit = repo.head.commit
+
 for diff_added in hcommit.diff("HEAD~1"):
     fileChanged=str(diff_added).split("==")[0].rstrip()
     ruta = path+"/"+fileChanged
     print("fichero->  "+path+"/"+fileChanged)
-
  
-
     with open(ruta, 'r') as file:
         field_list = yaml.safe_load(file)
 
